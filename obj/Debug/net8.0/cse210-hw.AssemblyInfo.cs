aefba0f59b7f85b6522ae677a02592bc199c1db1
melchy34 <uzu24001@byui.edu>
2025-01-20 21:26:59 +0100

    second commit

--- obj/Debug/net8.0/cse210-hw.AssemblyInfo.cs
+++ obj/Debug/net8.0/cse210-hw.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cse210-hw")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54aa7536a8678c0a8c0ab1d40f32bc6daf15b052")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ebe617ba17046a67a3480255789fb9abe23f00e")]
 [assembly: System.Reflection.AssemblyProductAttribute("cse210-hw")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cse210-hw")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
